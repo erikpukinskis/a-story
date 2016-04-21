90e8d03568a0df579056d7580a817180194e35a9
Erik Pukinskis <erik@snowedin.net>
2016-04-21 16:15:55 -0700

    formatting

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// GLOBAL API SHIT
+// GLOBAL API SHIT //////////
 
 
 // BAR CODE
@@ -116,67 +116,7 @@ function tryToRun(func) {
 }
 
 
-// OUR PROGRAM
-
-var program = {
-  kind: "function literal",
-  argumentNames: ["element", "bridgeRoute"],
-  body: [
-    {//////////
-      kind: "variable assignment",
-      variableName: "page",
-      expression: {
-        kind: "function call",
-        functionName: "element",
-        arguments: [
-          stringLiteralJson("sup family"),
-          // stringLiteralJson("body"),
-          {
-            kind: "function call",
-            functionName: "element.style",
-            arguments: [
-              {
-                kind: "object literal",
-                object:
-                  {
-                "background": stringLiteralJson("cornsilk"),
-                "color": stringLiteralJson("orchid"),
-                "font-size": stringLiteralJson("60pt"),
-                "font-family": stringLiteralJson("georgia")
-                  }
-              }
-            ]
-          }
-        ]
-      }
-    }, /////////////////
-    {
-      kind: "function call",
-      functionName: "bridgeRoute",
-      arguments: [
-        stringLiteralJson("/"),
-        {
-          kind: "function literal",
-          argumentNames: ["bridge"],
-          body: [
-        {
-          kind: "function call",
-          functionName: "bridge.sendPage",
-          arguments: [
-            {
-              kind: "variable reference",
-              variableName: "page"
-            }
-          ]
-        }
-          ]
-        }
-      ]
-    }//////////////////
-  ]
-}
-
-
+// SAMPLE PROGRAM //////////
 
 function stringLiteralJson(string) {
   return {
@@ -189,10 +129,70 @@ function emptyExpressionJson() {
   return { kind: "empty expression" }
 }
 
+var program = {
+  kind: "function literal",
+  argumentNames: ["element", "bridgeRoute"],
+  body: [
+{
+  kind: "variable assignment",
+  variableName: "page",
+  expression: {
+    kind: "function call",
+    functionName: "element",
+    arguments: [
+      stringLiteralJson("sup family"),
+      // stringLiteralJson("body"),
+{
+  kind: "function call",
+  functionName: "element.style",
+  arguments: [
+    {
+      kind: "object literal",
+      object:
+        {
+      "background": stringLiteralJson("cornsilk"),
+      "color": stringLiteralJson("orchid"),
+      "font-size": stringLiteralJson("60pt"),
+      "font-family": stringLiteralJson("georgia")
+        }
+    }
+  ]
+}
+    ]
+  }
+},
+{
+  kind: "function call",
+  functionName: "bridgeRoute",
+  arguments: [
+    stringLiteralJson("/"),
+    {
+      kind: "function literal",
+      argumentNames: ["bridge"],
+      body: [
+{
+  kind: "function call",
+  functionName: "bridge.sendPage",
+  arguments: [
+    {
+      kind: "variable reference",
+      variableName: "page"
+    }
+  ]
+}
+      ]
+    }
+  ]
+}
+  ]
+}
 
 
 
-// RENDERERS
+// THE EDITOR ////////////////
+
+
+// RENDERERS 
 
 var ghostExpression = element.template(
   ".ghost-expression.ghost.button",

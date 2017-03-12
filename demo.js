1caa97a9c5e43624d2f332231739cd431198e7ff
Erik Pukinskis <erik@snowedin.net>
2017-03-12 12:14:53 -0700

    simplify demo

--- demo.js
+++ demo.js
@@ -4,20 +4,9 @@ library.using(
   ["web-host", "an-expression", "./"],
   function(host, anExpression, renderExpression) {
 
-    var func = function(checkBook) {
-      sanscrit("boo", "ba", "loo")
-      sanscritic("boobaloo")
-
+    var func = function() {
       return function(bridge) {
-        var erik = checkBook("January paid")
-
-        paid("Feburary rent", "-$2085.00", "2/6/2017")
-        paid("Mom", "$100.00", "2/6/2017")
-        paid("Teensy house gutter", "-7.63", "2/7/2017")
-        paid("Sandwiches & Coffee", "-17.18", "2/6/2017")
-        paid.soon("Lyft to Marie", "-9.29", "2/6/2016")
-
-        erik.sendTo(bridge)
+        bridge.send("hello, world")
       }
     }
 
